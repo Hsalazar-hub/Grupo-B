aa98555c40304681d599cd611e744bd8fb6d1a11
lewis <lewisnn56@gmail.com>
2021-02-04 21:10:40 -0400

    comienzo taylor

--- calculo_proyecto_1.py
+++ calculo_proyecto_1.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import math
 """
 EJERCICIO 1: Implemente en lenguaje Python una función que aproxime mediante un Polinomio de Taylor de tercer orden centrado en x=1 para predecir f(2), siendo f(x) = 25x**3 – 6x**2 + 7x – 88. Calcule luego el error relativo de la aproximación.
 
@@ -8,3 +9,16 @@ Escriba aqui los nombres de los integrantes del grupo:
 -Hector Salazar. C.I 25967387 
 -Lewis Narvaez. C.I 26087715
 """
+
+def derivada(f, h = 0.01):
+    """
+    devuelve la función derivada de f dado un h.
+        Parametros:
+        f: función f(x).
+        h: tamaño del paso.
+    """
+
+    def _(x):
+        return (f(x + h) - f(x))/h
+
+    return _
